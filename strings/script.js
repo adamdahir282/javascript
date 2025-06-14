6f1f9b00e1f522b9bfb79005e91f30a648f356b6
adam dahir <adamdahir@MacBook-Air-van-adam.local>
2025-06-14 20:34:18 +0200

    i have learnt many ways of creating strings and using strings functions

---
+++ strings/script.js
@@ -0,0 +1,50 @@
+//different ways to create strings
+
+
+let firstName = "John J"; // using double quotes
+let lastName = 'Doe'; // using single quotes
+let age = 30;
+let isMarried = false;
+let city = new String("New York"); // using the String constructor
+let country = `USA`; // using template literals back ticks
+//console.log(firstName,lastName,city.toString(),country);
+
+
+// how to access strings
+
+// console.log(firstName.indexOf("J"));
+// console.log(firstName.indexOf("n"));
+// console.log(firstName.lastIndexOf("J"));
+
+
+// string methods
+let str = "Hello, world! \n";
+//console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+ //console.log(str.trim());
+// console.log(str.trimStart());
+ //console.log(str.trimEnd());
+// console.log(str.split(","));
+// console.log(str.split(" "));
+ //console.log(str.replace("World", "Earth"));
+ //console.log(str.includes("world"));
+//console.log(str.startsWith("Hello"));
+ //console.log(str.endsWith("!"));
+ //console.log(str.repeat(3));
+ //console.log(str.padStart(20, "i live in london and outside london "));
+// console.log(str.padEnd(20,"World "));
+// console.log(str.charAt(0));
+// console.log(str.charCodeAt(0));
+// console.log(str.slice(0,5));
+// console.log(str.substring(0,5));
+// console.log(str.split("").reverse().join(""));
+
+// if (str.includes("World")) {
+//     console.log("this string  has the word world in it!");
+// } else {
+//     console.log("it does not have the word world in it!");
+//     }
+
+
+    
