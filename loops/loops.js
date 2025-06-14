10755c19dbccb08d84a66b0703254d77e18e017a
adam dahir <adamdahir@MacBook-Air-van-adam.local>
2025-06-14 19:24:44 +0200

    my javascript work

---
+++ loops/loops.js
@@ -0,0 +1,6 @@
+ let towns = [ "hargeisa","xamar", "kismayo","b/hawa", "mandera", "nairobi","amsterdam","den haag"];
+ for (let i = 0; i < towns.length; i++) {
+    console.log (towns[i], `s index is ${i}`);
+   
+    
+ }
