70f7d083c503fafba4173da67e872fc6b43a9ce1
adam dahir <adamdahir@MacBook-Air-van-adam.local>
2025-06-15 18:59:29 +0200

    i have practiced loops

--- loops/loops.js
+++ loops/loops.js
@@ -9,3 +9,10 @@ let countries = ["somalia", "kenya", "tanzania", "uganda", "south sudan"];
 for (let index = 0; index < countries.length; index++) {
   console.log(countries);
 } // prints all countries in the array
+
+let cities = ["london", "paris", "rome", "madrid", "barcelona"];
+for (let i = 0; i < cities.length; i++) {
+   console.log(cities);}
+   
+
+
